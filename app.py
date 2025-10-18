dd25190de95084f582f44547542cad5ce41c237b
jingidoomaf <jingidoomaf@gmail.com>
2025-10-18 09:46:46 +0700

    Added welcome header to login page

--- app.py
+++ app.py
@@ -2,25 +2,16 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-users = []
-
 @app.route('/')
 def home():
-    return '<h1>Welcome to the Flask App</h1><a href="/login">Login</a> | <a href="/register">Register</a>'
+    return redirect(url_for('login'))
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login')
 def login():
-    if request.method == 'POST':
-        return redirect(url_for('home'))
     return render_template('login.html')
 
-@app.route('/register', methods=['GET', 'POST'])
+@app.route('/register')
 def register():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        users.append({'username': username, 'password': password})
-        return redirect(url_for('login'))
     return render_template('register.html')
 
 if __name__ == '__main__':
